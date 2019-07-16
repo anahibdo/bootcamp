4cccce2f662b4317070f2216f14eeaf9e65070d7
anahi <ducaslon@gmail.com>
2019-07-16 16:42:49 +0200

    primer archivo

---
+++ dia1.py
@@ -0,0 +1,64 @@
+print(2*2)
+print(3*4)
+print(12-4)
+print(12/4)
+print("HOLA MUNDO!")
+print ("2+2")
+print("HOLA "*10)
+a= 2
+b=10
+print(a+b)
+print("hola", 2, "chau", 10)
+nombre = "ANAHI"
+edad = 27
+print("hola, me llamo", nombre , "y tengo", edad )
+hobby = "bailar"
+print("hola, me llamo", nombre , "y tengo", edad, "y mi hobby es",hobby )
+lista_vacia=[] # con los corchetes se crea una lista vacia
+listax = [1,2,6,8] # lista con 4 elementos de tipo integer
+print(lista_vacia) #se imprime la lista vacia
+print(listax) #se imprime la lista listax
+datos=["ANAHI"] #se crea una lista datos con un elemento strin
+print(datos) #imprimos la lista anterior
+alumnos= ["Jose","Ramoncita", nombre, "Pepa"] #lista de
+print(alumnos)
+#nro_alumno = 3
+print(alumnos[2], alumnos [0])
+datos = ["ANAHI", 26]
+print("hola me llamo", datos[0], "y tengo", datos[1])
+datos[1] = 99
+print("hola me llamo", datos[0], "y tengo", datos[1])
+print(datos)
+datos.append("Programador")
+print(datos)
+print(datos[2])
+datos.append("Agronoma")
+datos.append ("ensenar")
+print(datos)
+datos.pop()
+print(datos)
+print(len(datos))
+saludo = "HOLA, QUE TAL"
+print(saludo)
+print(len(saludo))
+saludo = "BUENAS TARDES, QUE TAL"
+print(saludo)
+print(len(saludo))
+print("la lista de datos tiene", len(datos), "elementos")
+print(datos)
+Ultimo=len(datos)-1
+print("el ultimo elemento es", datos[Ultimo])
+lista_temas = ["variables", "listas", "tipo de datos"]
+for concepto in lista_temas:
+    print("hoy aprendi"), concepto
+    print("que gusto")
+    print("esto es lo que aprendi hoy")
+lista_temas = ["Elegancia", "moda", "novedades"]
+for concepto in lista_temas:
+    print(concepto)
+print("Lo bueno de ahora")
+
+for x in range(5):
+    print ("hola", x)
+for variable in range (1,100):
+    print("1,100", variable )
